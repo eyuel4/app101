d2deef66bda1586a4d0568e8d94b423bf2f2c1d9
Eyuel <eyuel.taddese4@gmail.com>
2018-04-01 23:14:24 -0400

    Working on MovieView

--- src/app/movies/movie-list/movies-list.component.ts
+++ src/app/movies/movie-list/movies-list.component.ts
@@ -9,51 +9,51 @@ import { MovieService } from "../../shared/service/movie.service";
     styleUrls: ['./movies-list.component.scss']
 })
 export class MoviesListComponent implements OnInit {
-    //movies : Movies[] //= [];
-    movies : Movies[] = [
-        new Movies(
-            100,
-            'Harry Potter',
-            'Good movie',
-            new Photo(12, 'HarryPotter', 'https://static.boredpanda.com/blog/wp-content/uploads/2016/01/16-year-old-artist-dimitra-milan-1.jpg')
-        ),
-        new Movies(
-            100,
-            'Harry Potter',
-            'Good movie',
-            new Photo(12, 'HarryPotter', 'https://static.boredpanda.com/blog/wp-content/uploads/2016/01/16-year-old-artist-dimitra-milan-1.jpg')
-        ),
-        new Movies(
-            100,
-            'Harry Potter',
-            'Good movie',
-            new Photo(12, 'HarryPotter', 'https://static.boredpanda.com/blog/wp-content/uploads/2016/01/16-year-old-artist-dimitra-milan-1.jpg')
-        ),
-        new Movies(
-            100,
-            'Harry Potter',
-            'Good movie',
-            new Photo(12, 'HarryPotter', 'https://static.boredpanda.com/blog/wp-content/uploads/2016/01/16-year-old-artist-dimitra-milan-1.jpg')
-        ),
-        new Movies(
-            100,
-            'Harry Potter',
-            'Good movie',
-            new Photo(12, 'HarryPotter', 'https://static.boredpanda.com/blog/wp-content/uploads/2016/01/16-year-old-artist-dimitra-milan-1.jpg')
-        ),
-        new Movies(
-            100,
-            'Harry Potter',
-            'Good movie',
-            new Photo(12, 'HarryPotter', 'https://static.boredpanda.com/blog/wp-content/uploads/2016/01/16-year-old-artist-dimitra-milan-1.jpg')
-        ),
-        new Movies(
-            100,
-            'Harry Potter',
-            'Good movie',
-            new Photo(12, 'HarryPotter', 'https://static.boredpanda.com/blog/wp-content/uploads/2016/01/16-year-old-artist-dimitra-milan-1.jpg')
-        )
-    ]
+    movies : Movies[] = [];
+    // movies : Movies[] = [
+    //     new Movies(
+    //         100,
+    //         'Harry Potter',
+    //         'Good movie',
+    //         new Photo(12, 'HarryPotter', 'https://static.boredpanda.com/blog/wp-content/uploads/2016/01/16-year-old-artist-dimitra-milan-1.jpg')
+    //     ),
+    //     new Movies(
+    //         100,
+    //         'Harry Potter',
+    //         'Good movie',
+    //         new Photo(12, 'HarryPotter', 'https://static.boredpanda.com/blog/wp-content/uploads/2016/01/16-year-old-artist-dimitra-milan-1.jpg')
+    //     ),
+    //     new Movies(
+    //         100,
+    //         'Harry Potter',
+    //         'Good movie',
+    //         new Photo(12, 'HarryPotter', 'https://static.boredpanda.com/blog/wp-content/uploads/2016/01/16-year-old-artist-dimitra-milan-1.jpg')
+    //     ),
+    //     new Movies(
+    //         100,
+    //         'Harry Potter',
+    //         'Good movie',
+    //         new Photo(12, 'HarryPotter', 'https://static.boredpanda.com/blog/wp-content/uploads/2016/01/16-year-old-artist-dimitra-milan-1.jpg')
+    //     ),
+    //     new Movies(
+    //         100,
+    //         'Harry Potter',
+    //         'Good movie',
+    //         new Photo(12, 'HarryPotter', 'https://static.boredpanda.com/blog/wp-content/uploads/2016/01/16-year-old-artist-dimitra-milan-1.jpg')
+    //     ),
+    //     new Movies(
+    //         100,
+    //         'Harry Potter',
+    //         'Good movie',
+    //         new Photo(12, 'HarryPotter', 'https://static.boredpanda.com/blog/wp-content/uploads/2016/01/16-year-old-artist-dimitra-milan-1.jpg')
+    //     ),
+    //     new Movies(
+    //         100,
+    //         'Harry Potter',
+    //         'Good movie',
+    //         new Photo(12, 'HarryPotter', 'https://static.boredpanda.com/blog/wp-content/uploads/2016/01/16-year-old-artist-dimitra-milan-1.jpg')
+    //     )
+    // ]
 
     constructor(private movieService: MovieService) {}
 
